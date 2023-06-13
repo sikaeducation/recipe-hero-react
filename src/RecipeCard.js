7dd8206d244b2aff19cf51b48cdd444b64d9040a
cw-roy <curtis_roy@msn.com>
2023-06-12 18:09:15 -0700

    Begin breaking out components

---
+++ src/RecipeCard.js
@@ -0,0 +1,13 @@
+import Reactfrom "react";
+
+const RecipeCard = () => {
+    return (
+      <li>
+        <div className="recipe-card">
+          {/* Content of the recipe card */}
+        </div>
+      </li>
+    );
+  };
+  
+  export default RecipeCard;
